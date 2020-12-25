50e6344ee08c5a6fee693985f18aa1ff71ca886e
Sara-otb <76565011+Sara-otb@users.noreply.github.com>
2020-12-25 18:25:20 +0300

    Done CW1

---
+++ Class Work 1/Class Work 1/Class_Work_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Class_Work_1App.swift
+//  Class Work 1
+//
+//  Created by لمى العتيبي on 24/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Class_Work_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
